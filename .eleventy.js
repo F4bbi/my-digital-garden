e79361022b97a2b254d3aede910752661a204d8a
F4bbi <75183735+F4bbi@users.noreply.github.com>
2023-04-07 00:54:16 +0200

    Delete src/site/index.11tydata.js
    Delete src/site/_data/filetree.js
    
    Update template file
    
    Update template file
    
    Update template file
    
    Update template file
    
    Update template file
    
    Update template file
    
    Update file .eleventy.js
    
    Update file README.md
    
    Update file package-lock.json
    
    Update file package.json
    
    Update file src/site/404.njk
    
    Update file src/site/feed.njk
    
    Update file src/site/styles/style.scss
    
    Update file src/site/styles/digital-garden-base.scss
    
    Update file src/site/styles/obsidian-base.scss
    
    Update file src/site/notes/notes.json
    
    Update file src/site/notes/notes.11tydata.js
    
    Update file src/site/_includes/layouts/note.njk
    
    Update file src/site/_includes/layouts/index.njk
    
    Update file src/site/_includes/components/pageheader.njk
    
    Update file src/site/_includes/components/linkPreview.njk
    
    Update file src/site/_includes/components/references.njk
    
    Update file src/site/_includes/components/sidebar.njk
    
    Update file src/site/_includes/components/graphScript.njk
    
    Update file src/site/_includes/components/filetree.njk
    
    Update file src/site/_includes/components/filetreeNavbar.njk
    
    Update file src/site/_includes/components/searchButton.njk
    
    Update file src/site/_includes/components/searchContainer.njk
    
    Update file src/site/_includes/components/searchScript.njk
    
    Update file src/site/_includes/components/calloutScript.njk
    
    Update file src/site/_includes/components/lucideIcons.njk
    
    Update file src/site/_includes/components/timestamps.njk
    
    Update file src/site/lunr.njk
    
    Update file src/site/_data/meta.js
    
    Update file src/site/_data/dynamics.js
    
    Update file src/helpers/filetreeUtils.js
    
    Update file src/helpers/linkUtils.js
    
    Update file src/site/get-theme.js
    
    Update file src/site/_data/eleventyComputed.js
    
    Update file src/site/graph.njk
    
    Updates

--- .eleventy.js
+++ .eleventy.js
@@ -3,18 +3,31 @@ const markdownIt = require("markdown-it");
 const fs = require("fs");
 const matter = require("gray-matter");
 const faviconPlugin = require("eleventy-favicon");
-const tocPlugin = require("eleventy-plugin-toc");
+const tocPlugin = require("eleventy-plugin-nesting-toc");
 const { parse } = require("node-html-parser");
+const htmlMinifier = require("html-minifier");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const { headerToId, namedHeadingsFilter } = require("./src/helpers/utils");
+const {
+  userMarkdownSetup,
+  userEleventySetup,
+} = require("./src/helpers/userSetup");
 
 const tagRegex = /(^|\s|\>)(#[^\s!@#$%^&*()=+\.,\[{\]};:'"?><]+)(?!([^<]*>))/g;
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true,
+  });
   let markdownLib = markdownIt({
     breaks: true,
     html: true,
   })
+    .use(require("markdown-it-anchor"), {
+      slugify: headerToId,
+    })
+    .use(require("markdown-it-mark"))
     .use(require("markdown-it-footnote"))
     .use(function (md) {
       md.renderer.rules.hashtag_open = function (tokens, idx) {
@@ -136,7 +149,8 @@ module.exports = function (eleventyConfig) {
 
         return defaultLinkRule(tokens, idx, options, env, self);
       };
-    });
+    })
+    .use(userMarkdownSetup);
 
   eleventyConfig.setLibrary("md", markdownLib);
 
@@ -150,7 +164,7 @@ module.exports = function (eleventyConfig) {
         }
         const [fileLink, linkTitle] = p1.split("|");
 
-        let fileName = fileLink;
+        let fileName = fileLink.replaceAll("&amp;", "&");
         let header = "";
         let headerLinkPath = "";
         if (fileLink.includes("#")) {
@@ -159,6 +173,7 @@ module.exports = function (eleventyConfig) {
         }
 
         let permalink = `/notes/${slugify(fileName)}`;
+        let noteIcon = process.env.NOTE_ICON_DEFAULT;
         const title = linkTitle ? linkTitle : fileName;
         let deadLink = false;
 
@@ -172,22 +187,19 @@ module.exports = function (eleventyConfig) {
           if (frontMatter.data.permalink) {
             permalink = frontMatter.data.permalink;
           }
+          if (frontMatter.data.tags && frontMatter.data.tags.indexOf("gardenEntry") != -1) {
+            permalink = "/";
+          }
+          if (frontMatter.data.noteIcon) {
+            noteIcon = frontMatter.data.noteIcon;
+          }
         } catch {
           deadLink = true;
         }
 
         return `<a class="internal-link ${
           deadLink ? "is-unresolved" : ""
-        }" href="${permalink}${headerLinkPath}">${title}</a>`;
-      })
-    );
-  });
-
-  eleventyConfig.addFilter("highlight", function (str) {
-    return (
-      str &&
-      str.replace(/\=\=(.*?)\=\=/g, function (match, p1) {
-        return `<mark>${p1}</mark>`;
+        }" ${deadLink ? "" : 'data-note-icon="' + noteIcon + '"'} href="${permalink}${headerLinkPath}">${title}</a>`;
       })
     );
   });
@@ -195,22 +207,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("taggify", function (str) {
     return (
       str &&
-      str.replace(
-        tagRegex,
-        function (match, precede, tag) {
-          return `${precede}<a class="tag" onclick="toggleTagSearch(this)">${tag}</a>`;
-        }
-      )
+      str.replace(tagRegex, function (match, precede, tag) {
+        return `${precede}<a class="tag" onclick="toggleTagSearch(this)" data-content="${tag}">${tag}</a>`;
+      })
     );
   });
 
   eleventyConfig.addFilter("searchableTags", function (str) {
     let tags;
-    let match =
-      str &&
-      str.match(
-        tagRegex,
-      );
+    let match = str && str.match(tagRegex);
     if (match) {
       tags = match
         .map((m) => {
@@ -223,6 +228,15 @@ module.exports = function (eleventyConfig) {
     } else {
       return "";
     }
+  });
+
+  eleventyConfig.addFilter("hideDataview", function (str) {
+    return (
+      str &&
+      str.replace(/\(\S+\:\:(.*)\)/g, function (_, value) {
+        return value.trim();
+      })
+    );
   });
 
   eleventyConfig.addTransform("callout-block", function (str) {
@@ -240,7 +254,7 @@ module.exports = function (eleventyConfig) {
         let calloutType = "";
         let isCollapsable;
         let isCollapsed;
-        const calloutMeta = /\[!(\w*)\](\+|\-){0,1}(\s?.*)/;
+        const calloutMeta = /\[!([\w-]*)\](\+|\-){0,1}(\s?.*)/;;
         if (!content.match(calloutMeta)) {
           continue;
         }
@@ -279,15 +293,57 @@ module.exports = function (eleventyConfig) {
     return str && parsed.innerHTML;
   });
 
+  eleventyConfig.addTransform("htmlMinifier", (content, outputPath) => {
+    if (
+      process.env.NODE_ENV === "production" &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      return htmlMinifier.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        keepClosingSlash: true,
+      });
+    }
+    return content;
+  });
+
   eleventyConfig.addPassthroughCopy("src/site/img");
+  eleventyConfig.addPassthroughCopy("src/site/scripts");
+  eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPlugin(faviconPlugin, { destination: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
     tags: ["h1", "h2", "h3", "h4", "h5", "h6"],
   });
+  eleventyConfig.addPlugin(pluginRss, {
+    posthtmlRenderOptions: {
+      closingSingleTag: "slash",
+      singleTags: ["link"]
+    }  
+  });
+
+  eleventyConfig.addFilter("dateToZulu", function(date){
+    if(!date) return "";
+    return new Date(date).toISOString("dd-MM-yyyyTHH:mm:ssZ");
+  });
   eleventyConfig.addFilter("jsonify", function (variable) {
     return JSON.stringify(variable) || '""';
   });
+
+  eleventyConfig.addFilter("validJson", function (variable) {
+    if (Array.isArray(variable)) {
+      return variable.map((x) => x.replaceAll("\\", "\\\\")).join(",");
+    } else if (typeof variable === "string") {
+      return variable.replaceAll("\\", "\\\\");
+    }
+    return variable;
+  });
+
+  userEleventySetup(eleventyConfig);
 
   return {
     dir: {
@@ -295,7 +351,7 @@ module.exports = function (eleventyConfig) {
       output: "dist",
       data: `_data`,
     },
-    templateFormats: ["njk", "md", "11ty.js", "css"],
+    templateFormats: ["njk", "md", "11ty.js"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     passthroughFileCopy: true,
